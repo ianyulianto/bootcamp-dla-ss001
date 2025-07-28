6109c959d2d6ec9b9a133a8f0e5ad265e49ea806
Ian Yulianto <ian.yulianto@live.com>
2025-07-28 18:49:44 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
